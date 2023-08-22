d7c04be1f99a65d6eac4a10497ba5679e8a15c99
Higor Morais de Lima <moraislimahigor@outlook.com>
2023-08-22 20:00:15 -0300

    Criando mais classes de modelo.

--- models.py
+++ models.py
@@ -11,6 +11,20 @@ class Cliente(db.Model):
     celular = db.Column(db.String(100))
     senha = db.Column(db.String(30))
     confsenha = db.Column(db.String(30))
+    
+class Comprador(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    nome = db.Column(db.String(100))
+    cpf = db.Column(db.String(11))
+
+class Venda(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    produto = db.relationship('Produto', backref='Venda', lazy=True, cascade="all") # D
+
+class Produto(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    nome = db.Column(db.String(100))
+    preco = db.Column(db.Double)
 
 # D
 class ClienteSchema(ma.Schema):
