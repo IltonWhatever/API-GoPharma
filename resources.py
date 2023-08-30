49e0b7e23d0a71f0335a7e3d8f90a3f53a9f1483
Ilton Whatever <ashpalete18@gmail.com>
2023-08-30 16:55:49 -0300

    Recursos para as classes Comprador e Venda

--- resources.py
+++ resources.py
@@ -1,7 +1,7 @@
 # Resources
 from flask_restful import Resource, reqparse, abort
 from flask import jsonify
-from models import db,Cliente,ClienteSchema,Produto,ProdutoSchema
+from models import db,Cliente,ClienteSchema,Produto,ProdutoSchema, Comprador, CompradorSchema, Venda,VendaSchema
 
 class ClienteResource(Resource):
     def get(self, cliente_id=None):
@@ -91,12 +91,12 @@ class ProdutoResource(Resource):
         db.session.commit()
 
         return jsonify(msg = {
-            "Resposta": "Cliente {} Deletado com Sucesso".format(produto_id)
+            "Resposta": "Produto {} Deletado com Sucesso".format(produto_id)
         })
 
     def put(self, produto_id=None):
         if produto_id is None:
-            abort(404, message="ID {} do Cliente não encontrado".format(produto_id))
+            abort(404, message="ID {} do Produto não encontrado".format(produto_id))
         
         parser = reqparse.RequestParser()
         parser.add_argument('nome', type=str, required=True)
@@ -113,16 +113,72 @@ class ProdutoResource(Resource):
         db.session.commit()
         return ProdutoSchema().dumps(produto)
 
-
-
 class CompradorResource(Resource):
-    parser = reqparse.RequestParser()
-    parser.add_argument('')
+    def get(self, comprador_id = None):
+        if comprador_id is None:
+            comprador = Comprador.querry.all
+            return CompradorSchema(many=True).dumps(comprador)
 
+        comprador = Comprador.query.get(comprador_id)
+        return CompradorSchema().dumps(comprador)
 
+    def post(self):
+        parser = reqparse.RequestParser()
+        parser.add_argument('nome', type=str, required=True)
+        parser.add_argument('cpf', type=str, required=True)
+        args = parser.parse_args()
+
+        comprador = Comprador(nome=args['nome'],
+        cpf=args['cpf']
+        )
+        db.session.add(comprador)
+        db.session.commit()
+        
+        return CompradorSchema().dumps(comprador)
+
+    def delete(self, comprador_id = None):
+        if comprador_id is None:
+            abort(404, message="ID {} do Comprador não encontrado".format(comprador_id))
+        Produto.query.filter_by(id=comprador_id).delete()
+        db.session.commit()
+
+        return jsonify(msg = {
+            "Resposta": "Cliente {} Deletado com Sucesso".format(comprador_id)
+        })
+
+    def put(self, comprador_id = None):
+        if comprador_id is None:
+            abort(404, message="ID {} do Comprador não encontrado".format(comprador_id))
+
+        parser = reqparse.RequestParser()
+        parser.add_argument('nome', type=str, required=True)
+        parser.add_argument('cpf', type=str, required=True)
+        args = parser.parse_args()
+        
+        comprador = Comprador.query.get(comprador_id)
+
+        comprador.nome = args['nome']
+        comprador.email = args['cpf']
+
+        db.session.commit()
+        return ClienteSchema().dump(comprador)
 
 class VendaResource(Resource):
     def get(self, venda_id = None):
         if venda_id is None:
             vendas = Venda.querry.all()
-            return ()
+            return VendaSchema().dumps(vendas)
+
+        venda = Venda.query.get(venda_id)
+        return VendaSchema().dumps(venda)
+
+    def put(self):
+        parser = reqparse.RequestParser()
+        parser.add_argument('compradorId', type=str, required=True)
+        args = parser.parse_args()
+
+        Venda = Cliente(nome=args['compradorId'])
+
+        db.session.add(Venda)
+        db.session.commit()
+        return ClienteSchema().dump(Venda)
